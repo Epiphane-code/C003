2dc5e52a52297ef135a3ac45d27da4fae8ceda18
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-23 03:19:48 +0100

    compte

---
+++ count_digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int count_digits(unsigned long long nb)
+{
+    int count = 0, test = nb/10;
+    while (test >= 1)
+    {
+        count = count + 1;
+        test = test/10;
+    }
+
+    return count;
+}
+
+int main()
+{
+   unsigned long long nb;
+   scanf("%llu", &nb);
+   printf("%llu contient %d chiffre(s).", nb, count_digits(nb));
+    
+}
